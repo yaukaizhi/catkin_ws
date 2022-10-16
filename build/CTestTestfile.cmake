1e74c143d01e5b8df905ad142b461a79cc104acb
yaukaizhi <kaizhi293@gmail.com>
2022-10-16 17:52:09 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kai/catkin_ws/src
+# Build directory: /home/kai/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tars4_1_urdf")
---
+++ build/tars4_1_urdf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kai/catkin_ws/src/tars4_1_urdf
+# Build directory: /home/kai/catkin_ws/build/tars4_1_urdf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
