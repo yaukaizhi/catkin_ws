823baf67b3e4e1d43fe052ca529a587e439d4df3
yaukaizhi <kaizhi293@gmail.com>
2022-10-18 15:00:19 +0800

    second commit

---
+++ src/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kai/catkin_ws/src/tars4_1_urdf
+# Build directory: /home/kai/catkin_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
