4ec1a8c2a8d2e33dc3b5375d72e71bd0d7b1eb4c
yaukaizhi <kaizhi293@gmail.com>
2022-12-05 19:39:31 +0800

    added weighted TARS5

---
+++ build/TARS5_WeightAssembly/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kai/catkin_ws/src/TARS5_WeightAssembly
+# Build directory: /home/kai/catkin_ws/build/TARS5_WeightAssembly
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/TARS_5_WeightAssembly/cmake_install.cmake
@@ -0,0 +1,70 @@
+# Install script for directory: /home/kai/catkin_ws/src/TARS_5_WeightAssembly
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/kai/catkin_ws/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/kai/catkin_ws/build/TARS_5_WeightAssembly/catkin_generated/installspace/TARS_5_WeightAssembly.pc")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly/cmake" TYPE FILE FILES
+    "/home/kai/catkin_ws/build/TARS_5_WeightAssembly/catkin_generated/installspace/TARS_5_WeightAssemblyConfig.cmake"
+    "/home/kai/catkin_ws/build/TARS_5_WeightAssembly/catkin_generated/installspace/TARS_5_WeightAssemblyConfig-version.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly" TYPE FILE FILES "/home/kai/catkin_ws/src/TARS_5_WeightAssembly/package.xml")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly/config" TYPE DIRECTORY FILES "/home/kai/catkin_ws/src/TARS_5_WeightAssembly/config/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly/launch" TYPE DIRECTORY FILES "/home/kai/catkin_ws/src/TARS_5_WeightAssembly/launch/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly/meshes" TYPE DIRECTORY FILES "/home/kai/catkin_ws/src/TARS_5_WeightAssembly/meshes/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/TARS_5_WeightAssembly/urdf" TYPE DIRECTORY FILES "/home/kai/catkin_ws/src/TARS_5_WeightAssembly/urdf/")
+endif()
+
